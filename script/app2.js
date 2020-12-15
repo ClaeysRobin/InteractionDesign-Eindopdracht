3c8dfbd915a277c6d85937ecc9ec858b1c681dc1
ClaeysRobin <55786806+ClaeysRobin@users.noreply.github.com>
2020-12-15 21:43:41 +0100

    commit

--- script/app2.js
+++ script/app2.js
@@ -5,8 +5,43 @@ const key = 'j7Ek11xfTcE2ugN1xo8iEwdrZXOnTsSQVPwQjYoY'
 
 
 let showdetails = queryResponse => {
-	// console.log({queryResponse});
-	// console.log(`${queryResponse.near_earth_objects[0].close_approach_data[0].miss_distance.lunar}`)
+	// console.log("show details");
+	// console.log(`${queryResponse.near_earth_objects[0].close_approach_data[0].close_approach_date}`)
+
+	
+
+	
+
+	const urlParams = new URLSearchParams(window.location.search);
+	const id = urlParams.get('id');
+	// console.log(id)
+
+	const naam = `${queryResponse.near_earth_objects[id].name}`
+	const naamltd = `${queryResponse.near_earth_objects[id].name_limited}`
+	const hazardous = `${queryResponse.near_earth_objects[id].is_potentially_hazardous_asteroid}`
+
+	// close aproach date uit json halen
+	// ${queryResponse.near_earth_objects[0].close_approach_data[0].close_approach_date}
+
+
+	document.querySelector(`.js-astroid-name`).innerHTML = naam;
+	document.querySelector(`.js-astroid-namelimited`).innerHTML = naamltd;
+	document.querySelector(`.js-astroid-hazardous`).innerHTML = hazardous;
+
+
+	const close_approach_data = queryResponse.near_earth_objects[id].close_approach_data
+	console.log(close_approach_data.length)
+
+	for (i = 0; i < close_approach_data.length; i++){
+		const dates = `${queryResponse.near_earth_objects[id].close_approach_data[i].close_approach_date_full}`
+
+		const velocity = `${queryResponse.near_earth_objects[id].close_approach_data[i].relative_velocity.kilometers_per_second}`
+
+		document.querySelector(`.js-astroid-approach`).innerHTML += `<p>${dates}  </p>`;
+		document.querySelector(`.js-astroid-velocity`).innerHTML += `<p>${velocity} Km / Seconde  </p>`;
+
+	}
+
 
 	
 };
@@ -31,7 +66,7 @@ let showResult = queryResponse => {
 		// plaats de astroiden
 		space.setAttribute('style', `bottom: ${afstand/200000}px; left:  ${leftspace += 9}%;`);
 		
-		space.innerHTML = `<a href="detail.html"><img src="img/png/astroid.png" alt="astroid" class="c-astroid"></a>`
+		space.innerHTML = `<a href="detail.html?id=${i}"><img src="img/png/astroid.png" alt="astroid" class="c-astroid"></a>`
 	}
 };
 
@@ -48,11 +83,18 @@ const getAPI = async () => {
 	//console.log(data);
 	
 	// Als dat gelukt is, gaan we naar onze showResult functie.
-	showResult(data);
+	if (window.location.pathname == "/home.html"){
+		showResult(data);
+	  }
+
+	if (window.location.pathname == "/detail.html"){
+      showdetails(data);
+    }
 };
 
 document.addEventListener('DOMContentLoaded', function() {
 	console.log("dom loaded")
 	// 1 We will query the API.
 	getAPI();
+
 });
